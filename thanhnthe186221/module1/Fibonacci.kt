01e0268fe5a5c65d99ad34d2e4e34da5918f70f5
thanhnthe186221 <tienthanh22092004@gmail.com>
2026-01-29 14:51:44 +0700

    Implement Fibonacci recursive

--- thanhnthe186221/module1/Fibonacci.kt
+++ thanhnthe186221/module1/Fibonacci.kt
@@ -1,5 +1,9 @@
 package thanhnthe186221.module1
 
-interface Fibonacci {
-    fun fibonacci(n: Int, flag: Boolean): Int
+class FibonacciImpl1 : Fibonacci {
+    override fun fibonacci(n: Int, flag: Boolean): Int {
+        if (!flag) return -1
+        if (n <= 1) return n
+        return fibonacci(n - 1, true) + fibonacci(n - 2, true)
+    }
 }

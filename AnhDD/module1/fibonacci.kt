97e56084ee2007d10004455bfce560e2969ce198
dduyanh27 <dduyanh2707@gmail.com>
2026-02-01 00:23:42 +0700

    Commit file AnhDD
    Fibo

---
+++ AnhDD/module1/fibonacci.kt
@@ -0,0 +1,18 @@
+class Fibonacci {
+    fun calculate(n: Int, flag: Boolean): Int {
+        if (flag) {
+            if (n <= 1) return n
+            return calculate(n - 1, true) + calculate(n - 2, true)
+        } else {
+            if (n <= 1) return n
+            var a = 0
+            var b = 1
+            for (i in 2..n) {
+                val sum = a + b
+                a = b
+                b = sum
+            }
+            return b
+        }
+    }
+}

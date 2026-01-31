f18e13065f5b4f7032c241c516dfd95e08519b81
BaoHGHE153305 <baohghe153305@gmail.com>
2026-01-31 20:36:04 +0700

    Implement Fibonacci Loop
    Implemented Fibonacci calculation using a for loop.

--- BaoHG/module1/fibonacci.kt
+++ BaoHG/module1/fibonacci.kt
@@ -1,5 +1,17 @@
 class Fibonacci {
     fun calculate(n: Int, flag: Boolean): Int {
-        return 0 // Placeholder
+        if (!flag) {
+            // Logic Vòng lặp for
+            if (n <= 1) return n
+            var a = 0
+            var b = 1
+            for (i in 2..n) {
+                val sum = a + b
+                a = b
+                b = sum
+            }
+            return b
+        }
+        return 0
     }
 }
